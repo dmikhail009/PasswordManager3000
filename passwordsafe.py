3c097e1cc9ae87d60d7328bac795f254a6c9afa2
Daniel Mikhail <daniel.mikhail009@gmail.com>
2021-01-02 12:22:08 -0700

    Explicitly creates db file in root folder Changed all password entries to show as bullets Added functions to "moving" buttons to clear entry fields on prev page Cleaned up GUI layout Cleaned up comments

--- passwordsafe.py
+++ passwordsafe.py
@@ -1,3 +1,5 @@
+# Imports
+from pathlib import Path
 import sqlite3, time, base64, hashlib
 from cryptography.hazmat.primitives import hashes
 from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
@@ -5,30 +7,30 @@ from cryptography.fernet import Fernet
 import tkinter as tk
 from tkinter import messagebox
 
+# Creates path for database in .py folder
+dbname = "PasswordManager3000"
+dbpath = (Path(__file__).parent / dbname).with_suffix('.db')
 # Establish connection with database
-conn = sqlite3.connect("pass_safe.db")
+conn = sqlite3.connect(dbpath)
 conn.row_factory = sqlite3.Row
 cur = conn.cursor()
-
 #Create user and password tables in database
 cur.execute("CREATE TABLE IF NOT EXISTS users (userid NUMERIC, email TEXT, password TEXT)")
 cur.execute("CREATE TABLE IF NOT EXISTS passwords (userid NUMERIC, app TEXT, username TEXT, email TEXT, password TEXT)")
 # Set encoding for program
 encoding = 'utf-8'
-
-# Initiating global user variables to access and encrypt/decrypt user passwords
+# Initiate global user variables to access and encrypt/decrypt user passwords
 userid = None
 key = None
 f = None
 
-# Function to generate encryption key and password hash form password string: returns key, f (for Fernet encryption)
+# Function to generate encryption key and password hash from password string: returns key, f (for Fernet encryption)
 def setkey(password):
     salt = hashlib.sha256(password.encode(encoding)).hexdigest()
     kdf = PBKDF2HMAC(
         algorithm = hashes.SHA256(),length=32,salt=bytes(salt.encode(encoding)),iterations=10000,
     )
     key = base64.urlsafe_b64encode(kdf.derive(password.encode(encoding)))
-    #pass_hash = hashlib.sha256(key).hexdigest()
     f = Fernet(key)
     return key, f
 
@@ -41,41 +43,42 @@ class PassManager (tk.Tk):
         container.pack(side="top", fill="both", expand=True)
         container.grid_rowconfigure(0, weight=1)
         container.grid_columnconfigure(0, weight=1)
-
         self.frames = {}
-        # Create frames and stack frames for app
-        for F in (LoginPage, RegisterPage, MainPage, AddPage, FindPage, DeletePage):
+        # Create and stack frames for app
+        for F in (SignInPage, SignUpPage, MainPage, AddPage, FindPage, DeletePage):
             frame = F(parent=container, controller=self)
             self.frames[F] = frame
             frame.grid(row=0, column=0, sticky="nsew")
         # Show LoginPage frame first
-        self.show_frame(LoginPage)
+        self.show_frame(SignInPage)
     # Function to raise called frame to top (show user)
     def show_frame(self, cont):
         frame = self.frames[cont]
         frame.tkraise()
 
-# Create LoginPage
-class LoginPage(tk.Frame):
+# Create SignInPage
+class SignInPage(tk.Frame):
     def __init__(self, parent, controller):
         tk.Frame.__init__(self, parent)
         self.controller = controller
         self.grid_rowconfigure([0, 1, 2, 3, 4], weight=1)
-        self.grid_columnconfigure([0, 1], weight=1)                
+        self.grid_columnconfigure([0, 1, 2], weight=1)                
+        lbl_header = tk.Label(self, text="Sign In", font=("Calibri", 14))
+        lbl_header.grid(row=0, column=1, columnspan=2, sticky='ew')
         lbl_email = tk.Label(self, text="Email:")
         ent_email = tk.Entry(self, width=40)
         lbl_email.grid(row=1,column=0, sticky='e')
-        ent_email.grid(row=1,column=1)
+        ent_email.grid(row=1,column=1, columnspan=2)
         lbl_password = tk.Label(self, text="Password:")
-        ent_password = tk.Entry(self, width=40)
+        ent_password = tk.Entry(self, show=u"\u2022", width=40)
         lbl_password.grid(row=2,column=0, sticky='e')
-        ent_password.grid(row=2,column=1)
-        btn_login = tk.Button(self, text="Login", command=lambda: self.login(ent_email, ent_password))
-        btn_login.grid(row=3,column=1, pady=5)
-        btn_register = tk.Button(self, text="Don't have an account?\nRegister", command=lambda: controller.show_frame(RegisterPage))      
-        btn_register.grid(row=4,column=1, pady=5)
-    # Login Function
-    def login(self, ent_email, ent_password):
+        ent_password.grid(row=2,column=1, columnspan=2)
+        btn_signin = tk.Button(self, text="Sign In", command=lambda: self.signin(ent_email, ent_password))
+        btn_signin.grid(row=4,column=1, sticky='ew')
+        btn_signup = tk.Button(self, text="Sign Up", command=lambda: self.gotosignup(ent_email, ent_password))      
+        btn_signup.grid(row=4,column=2, padx=10, sticky='ew')
+    # Sign In Function
+    def signin(self, ent_email, ent_password):
         global userid
         global key
         global f
@@ -92,45 +95,55 @@ class LoginPage(tk.Frame):
                 ent_email.delete(0,"end")
                 ent_password.delete(0,"end")
             else:
-                messagebox.showerror("Login Error", "Incorrect password entered")
+                messagebox.showerror("Sign In Error", "Incorrect password entered")
         else:
-            messagebox.showerror("Login Error", "No account could be found for this email")      
+            messagebox.showerror("Sign In Error", "No account could be found for this email")
+    def gotosignup(self, ent_email, ent_password):
+        self.controller.show_frame(SignUpPage)
+        ent_email.delete(0,"end")
+        ent_password.delete(0,"end")
 
-# Create RegisterPage
-class RegisterPage(tk.Frame):
+# Create SignUpPage
+class SignUpPage(tk.Frame):
     def __init__(self, parent, controller):
         tk.Frame.__init__(self, parent)
         self.controller = controller
         self.grid_rowconfigure([0, 1, 2, 3], weight=1)
         self.grid_columnconfigure([0, 1], weight=1)
+        lbl_header = tk.Label(self, text="Sign Up", font=("Calibri", 14))
+        lbl_header.grid(row=0, column=1, sticky='ew')
         lbl_email = tk.Label(self, text="Email:")
         ent_email = tk.Entry(self, width=40)
         lbl_email.grid(row=1,column=0, sticky='e')
-        ent_email.grid(row=1,column=1)
+        ent_email.grid(row=1,column=1, columnspan=3)
         lbl_password = tk.Label(self, text="Password:")
-        ent_password = tk.Entry(self, width=40)
+        ent_password = tk.Entry(self, show=u"\u2022", width=40)
         lbl_password.grid(row=2,column=0, sticky='e')
-        ent_password.grid(row=2,column=1)
+        ent_password.grid(row=2,column=1, columnspan=3)
         lbl_confirmation = tk.Label(self, text="Confirmation:")
-        ent_confirmation = tk.Entry(self, width=40)
+        ent_confirmation = tk.Entry(self, show=u"\u2022", width=40)
         lbl_confirmation.grid(row=3,column=0, sticky='e')
-        ent_confirmation.grid(row=3,column=1)
-        btn_register = tk.Button(self, text="Register", command=lambda: self.register(ent_email, ent_password, ent_confirmation))
-        btn_register.grid(row=4,column=1, pady=5)
-    # Register Function
-    def register(self, ent_email, ent_password, ent_confirmation):
+        ent_confirmation.grid(row=3,column=1, columnspan=3)
+        btn_signup = tk.Button(self, text="Sign Up", command=lambda: self.signup(ent_email, ent_password, ent_confirmation))
+        btn_signup.grid(row=4,column=1, padx=10, pady=5, sticky='ew')
+        btn_back = tk.Button(self, text="Back", command=lambda: self.gotosignin(ent_email, ent_password, ent_confirmation))
+        btn_back.grid(row=4,column=0, padx=10, pady=5, sticky='ew')
+    # Sign Up Function
+    def signup(self, ent_email, ent_password, ent_confirmation):
         global userid
         global key
         global f
         email = ent_email.get().lower()
         password = ent_password.get()
         confirmation = ent_confirmation.get()
+        if email == "" or password == "":
+            messagebox.showerror("Sign Up Error", "Please enter an email and password to register an account ")
         # Checks entered password and confirmation match
-        if password != confirmation:
-            messagebox.showerror("Registration Error", "Entered passwords do not match")
+        elif password != confirmation:
+            messagebox.showerror("Sign Up Error", "Entered passwords do not match")
         # Checks email does not have an accout already in users
         elif len(cur.execute("SELECT * FROM users WHERE email = :email", {"email": email}).fetchall()):
-            messagebox.showerror("Registration Error", "This email is already registered with an account")
+            messagebox.showerror("Sign Up Error", "This email is already registered with an account")
         else:
             # If first ever account, assigns userid as 0
             if len(cur.execute("SELECT * from users WHERE email = :email", {"email": email}).fetchall()) == 0:
@@ -141,11 +154,16 @@ class RegisterPage(tk.Frame):
             key, f = setkey(password)
             cur.execute("INSERT INTO users (userid, email, password) VALUES (:userid, :email, :password)", {"userid": userid, "email": email, "password": hashlib.sha256(password.encode(encoding)).hexdigest()})
             conn.commit()
-            messagebox.showinfo("Registration Complete", "Welcome! "+email+" is now registered.\nTo begin using Software Manager 3000, please Login")
-            self.controller.show_frame(LoginPage)
+            messagebox.showinfo("Sign Up Complete", "Welcome! "+email+" is now registered.\nTo begin using PasswordManager3000, please Login")
+            self.controller.show_frame(SignInPage)
             ent_email.delete(0,"end")
             ent_password.delete(0,"end")
             ent_confirmation.delete(0,"end")
+    def gotosignin(self, ent_email, ent_password, ent_confirmation):
+        self.controller.show_frame(SignInPage)
+        ent_email.delete(0,"end")
+        ent_password.delete(0,"end")
+        ent_confirmation.delete(0,"end")
 
 # Create MainPage
 class MainPage(tk.Frame):
@@ -154,47 +172,49 @@ class MainPage(tk.Frame):
         self.controller = controller
         self.grid_rowconfigure([0, 1, 2, 3, 4, 5], weight=1)
         self.grid_columnconfigure([0, 1], weight=1)
+        lbl_header = tk.Label(self, text="Select Option", font=("Calibri", 14))
+        lbl_header.grid(row=0, column=0, columnspan=2, sticky='ew')
         btn_add = tk.Button(self, text="ADD", command=lambda: controller.show_frame(AddPage))
         lbl_add = tk.Label(self, text="a new account")
-        btn_add.grid(row=1,column=0, sticky='ew')
+        btn_add.grid(row=1,column=0, padx=10, sticky='ew')
         lbl_add.grid(row=1,column=1, sticky='w')
         btn_find = tk.Button(self, text="FIND", command=lambda: controller.show_frame(FindPage))
         lbl_find = tk.Label(self, text="a password for existing account")
-        btn_find.grid(row=2,column=0, sticky='ew')
+        btn_find.grid(row=2,column=0, padx=10, sticky='ew')
         lbl_find.grid(row=2,column=1, sticky='w')
         btn_list = tk.Button(self, text="LIST", command=lambda: self.listf())
         lbl_list = tk.Label(self, text="accounts registered to a username/email")
-        btn_list.grid(row=3,column=0, sticky='ew')
+        btn_list.grid(row=3,column=0, padx=10, sticky='ew')
         lbl_list.grid(row=3,column=1, sticky='w')
         btn_delete = tk.Button(self, text="DELETE", command=lambda: controller.show_frame(DeletePage))
         lbl_delete = tk.Label(self, text="account information")
-        btn_delete.grid(row=4,column=0, sticky='ew')
+        btn_delete.grid(row=4,column=0, padx=10, sticky='ew')
         lbl_delete.grid(row=4,column=1, sticky='w')
-        btn_logout = tk.Button(self, text="Logout", command=lambda: self.logout())
-        btn_logout.grid(row=5,column=1, pady=5)
-    # Logout Function
-    def logout(self):
+        btn_signout = tk.Button(self, text="Sign Out", command=lambda: self.signout())
+        btn_signout.grid(row=5,column=0, padx=10, pady=5, sticky='ew')
+    # Sign Out Function
+    def signout(self):
         global userid
         global key
         global f
-        # Clear user information and return to LoginPage
+        # Clear user information and return to SignInPage
         userid = None
         key = None
         f = None
-        messagebox.showinfo("Logged Out", "You have been successfully logged out")
-        self.controller.show_frame(LoginPage)
+        messagebox.showinfo("Signed Out", "You have been successfully signed out")
+        self.controller.show_frame(SignInPage)
     # List Function
     def listf(self):
         global userid
         rows = cur.execute("SELECT * FROM passwords where userid = :userid", {"userid": userid}).fetchall()
         if len(rows) == 0:
-            messagebox.showerror("Search Error", "No accounts under found")
+            messagebox.showerror("Search Error", "No accounts found")
         else:
             apps = str()
             for row in rows:
                 app = row["app"]
                 apps = apps+"\n"+app
-            messagebox.showinfo("Search Results", "The following account were found:\n"+apps)
+            messagebox.showinfo("Search Results", "The following accounts were found:\n"+apps)
 
 # Create AddPage
 class AddPage(tk.Frame):
@@ -203,6 +223,8 @@ class AddPage(tk.Frame):
         self.controller = controller
         self.grid_rowconfigure([0, 1, 2, 3, 4, 5, 6], weight=1)
         self.grid_columnconfigure([0, 1], weight=1)
+        lbl_header = tk.Label(self, text="Add Account", font=("Calibri", 14))
+        lbl_header.grid(row=0, column=1, sticky='ew')
         lbl_app = tk.Label(self, text="Account/App Name:")
         ent_app = tk.Entry(self, width=40)
         lbl_app.grid(row=1,column=0, sticky='e')
@@ -224,10 +246,10 @@ class AddPage(tk.Frame):
         lbl_confirmation.grid(row=5,column=0, sticky='e')
         ent_confirmation.grid(row=5,column=1)
         btn_submit = tk.Button(self, text="Submit", command=lambda: self.add(ent_app, ent_username, ent_email, ent_password, ent_confirmation))
-        btn_submit.grid(row=6, column=1, pady=5)
-        btn_main = tk.Button(self, text="Main Page", command=lambda: controller.show_frame(MainPage))
-        btn_main.grid(row=6, column=0, pady=5)
-    # Add Function
+        btn_submit.grid(row=6, column=1, padx=10, pady=5, sticky='ew')
+        btn_back = tk.Button(self, text="Back", command=lambda: self.gotomain(ent_app, ent_username, ent_email, ent_password, ent_confirmation))
+        btn_back.grid(row=6, column=0, padx=10, pady=5, sticky='ew')
+    # Add account Function
     def add(self, ent_app, ent_username, ent_email, ent_password, ent_confirmation):
         global userid
         global key
@@ -254,6 +276,13 @@ class AddPage(tk.Frame):
             ent_email.delete(0,"end")
             ent_password.delete(0,"end")
             ent_confirmation.delete(0,"end")
+    def gotomain(self, ent_app, ent_username, ent_email, ent_password, ent_confirmation):
+        self.controller.show_frame(MainPage)
+        ent_app.delete(0, "end")
+        ent_username.delete(0, "end")
+        ent_email.delete(0,"end")
+        ent_password.delete(0,"end")
+        ent_confirmation.delete(0,"end")
 # Create FindPage
 class FindPage(tk.Frame):
     def __init__(self, parent, controller):
@@ -261,15 +290,17 @@ class FindPage(tk.Frame):
         self.controller = controller
         self.grid_rowconfigure([0, 1, 2], weight=1)
         self.grid_columnconfigure([0, 1], weight=1)
+        lbl_header = tk.Label(self, text="Find Password", font=("Calibri", 14))
+        lbl_header.grid(row=0, column=1, sticky='ew')
         lbl_app = tk.Label(self, text="Account/App Name:")
         ent_app = tk.Entry(self, width=40)
         lbl_app.grid(row=1,column=0, sticky='e')
         ent_app.grid(row=1,column=1)
         btn_submit = tk.Button(self, text="Submit", command=lambda: self.find(ent_app))
-        btn_submit.grid(row=2, column=1, pady=5)
-        btn_main = tk.Button(self, text="Main Page", command=lambda: controller.show_frame(MainPage))
-        btn_main.grid(row=6, column=0, pady=5)
-    # Find Function
+        btn_submit.grid(row=2, column=1, padx=10, pady=5, sticky='ew')
+        btn_back = tk.Button(self, text="Back", command=lambda: self.gotomain(ent_app))
+        btn_back.grid(row=2, column=0, padx=10, pady=5, sticky='ew')
+    # Find password Function
     def find(self, ent_app):
         global userid
         global key
@@ -285,6 +316,9 @@ class FindPage(tk.Frame):
             messagebox.showinfo("Search Results", "Account info for "+app+":\nEmail: "+email+"\nUsername: "+username+"\nPassword: "+password)
             self.controller.show_frame(MainPage)
             ent_app.delete(0,"end")
+    def gotomain(self, ent_app):
+        self.controller.show_frame(MainPage)
+        ent_app.delete(0, "end")
 
 # Create DeletePage
 class DeletePage(tk.Frame):
@@ -293,15 +327,17 @@ class DeletePage(tk.Frame):
         self.controller = controller
         self.grid_rowconfigure([0, 1, 2], weight=1)
         self.grid_columnconfigure([0, 1], weight=1)
+        lbl_header = tk.Label(self, text="Delete Account", font=("Calibri", 14))
+        lbl_header.grid(row=0, column=1, sticky='ew')
         lbl_app = tk.Label(self, text="Account/App Name:")
         ent_app = tk.Entry(self, width=40)
         lbl_app.grid(row=1,column=0, sticky='e')
         ent_app.grid(row=1,column=1)
         btn_delete = tk.Button(self, text="Delete", command=lambda: self.delete(ent_app))
-        btn_delete.grid(row=2, column=1, pady=5)
-        btn_main = tk.Button(self, text="Main Page", command=lambda: controller.show_frame(MainPage))
-        btn_main.grid(row=6, column=0, pady=5)
-    # Delete Function
+        btn_delete.grid(row=2, column=1, padx=10, pady=5, sticky='ew')
+        btn_main = tk.Button(self, text="Back", command=lambda: self.gotomain(ent_app))
+        btn_main.grid(row=2, column=0, padx=10, pady=5, sticky='ew')
+    # Delete account Function
     def delete(self, ent_app):
         global userid
         app = ent_app.get().lower()
@@ -313,6 +349,9 @@ class DeletePage(tk.Frame):
             messagebox.showinfo("Delete Confirmation", "Account info for "+app+" was successfully deleted")
             self.controller.show_frame(MainPage)
             ent_app.delete(0,"end")
+    def gotomain(self, ent_app):
+        self.controller.show_frame(MainPage)
+        ent_app.delete(0, "end")
 
 app = PassManager()
 app.mainloop()
